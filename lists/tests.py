1ae77f2c5690a0dd75d597ae8bdcefd0e5e7c35a
k5766273 <72492462+k5766273@users.noreply.github.com>
2021-05-07 13:28:18 +0800

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -1,61 +1,29 @@
-'''
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
-
+from lists.models import Item
 from lists.views import home_page
-class HomePageTest(TestCase):
-    def test_root_url_resolve_to_home_page_view(self):
-        found=resolve('/')
-        self.assertEqual(found.func,home_page)
-    def test_home_page_returns_correct_html(self):
-        request=HttpRequest()
-        response=home_page(request)
-        html=response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>',html)
-        self.assertTrue(html.endswith('</html>'))
-# Create your tests here.
-'''
-from django.urls import resolve
-from django.test import TestCase
-from lists.views import home_page
-from django.http import HttpRequest
-from django.template.loader import render_to_string
-
 
 class HomePageTest(TestCase):
-
-    def test_use_home_template(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
-     #   self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),'New To-Do item did not appear in table')
-
+        self.assertTemplateUsed(response, 'home.html')
+    def test_can_save_a_post_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
 
-'''
-class HomePageTest(TestCase):
-
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')      #解析url，映射到响应的视图函数
-        self.assertEqual(found.func,home_page)
-
-    def test_home_page_returns_correct_html(self):
-        response=self.client.get('/')
-        html=response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>',html)
-        self.assertTrue(html.strip().endswith('</html>'))
-        self.assertTemplateUsed(response,'home.html')
-        '''
-'''
-        request=HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        expected_html=render_to_string('home.html')
-        self.assertEqual(html,expected_html)
-   
-     
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>',html)
-        self.assertTrue(html.endswith('</html>'))
-'''
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
--- lists/views.py
+++ lists/views.py
@@ -1,12 +1,8 @@
-#from django.shortcuts import render
-from django.http import HttpResponse
 from django.shortcuts import render
-#home_page=None
+from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-    return render(request,'home.html')
-'''
-def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
-'''
+    return render(request, 'home.html',{
+        'new_item_text': request.POST.get('item_text', ''),
+        })
